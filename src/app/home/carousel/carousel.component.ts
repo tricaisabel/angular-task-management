6b0ea3b82afdad73892e993b18da8d7d192227e5
Isabel Trica <isabel.trica@bearingpoint.com>
2023-03-08 10:19:06 +0200

    carousel changes

--- src/app/home/carousel/carousel.component.ts
+++ src/app/home/carousel/carousel.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable, Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
 
+  tabIndex = 0;
+
+  ngOnInit() {
+    const source = timer(5000, 5000);
+    this.subscription = source.subscribe((val) => this.changeTabIndex(1));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  public changeTabIndex(value: number) {
+    const tabCount = 3;
+    if (this.tabIndex + value < 0) {
+      this.tabIndex = 3;
+    }
+    this.tabIndex = (this.tabIndex + value) % tabCount;
+  }
+
+  public arrowClicked(value: number) {
+    this.changeTabIndex(value);
+    this.subscription.unsubscribe();
+  }
 }
